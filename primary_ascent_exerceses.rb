431b0c6ea433a16ab621e1f89198ec4364afd323
llwang8 <wangxbl56@gmail.com>
2016-01-29 22:53:27 -0500

    Primer Ascent - exercises

--- primary_ascent_exerceses.rb
+++ primary_ascent_exerceses.rb
@@ -4,11 +4,26 @@
 #forms the inheritance hierarchy of the object. The methods added to an object by a class/module 
 #an however be overriden by others which are below them in the inheritance chain.
 
-
-
-
-
+def location_in_hierarchy(object, method)
+  # Your code here
+     klass = object.class
+     ancesters = [klass]
+       while  !(superclass = klass.superclass).nil?
+       ancesters << superclass
+       klass = superclass
+     end
+     
+       ancesters.reverse.find do |a|
+         a.instance_methods.include? method
+     end 
+end
 
 #Problem Statement
 #Compute the sum of cubes for a given range a through b.
+
+def sum_of_cubes(a, b)
+  # Write your code here
+  (a..b).inject(0) {|sum, i| sum + i ** 3}
+  
+end
 
